e620ee2c9dc8aea96538cdc5528c2ba72ce6f4d3
FilipPedersen <60428645+FilipPedersen@users.noreply.github.com>
2024-06-03 22:59:01 +0200

    add: user signed up endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { User } from '@prisma/client';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService) {}
 
-  @Get()
-  getUsers() {
-    return this.usersService.getUsers();
-  }
+    @Get()
+    getUsers() {
+        return this.usersService.getUsers();
+    }
+
+    @Post('user-signed-up')
+    @HttpCode(201)
+    async handleUserSignedUp(
+        @Body() userData: { clerkId: string; email: string; id: string },
+    ): Promise<User> {
+        return this.usersService.createUser(userData);
+    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, User } from '@prisma/client';
 const prisma = new PrismaClient();
 
 @Injectable()
 export class UsersService {
-  constructor() {}
-  async getUsers() {
-    const users = prisma.user.findMany();
-    console.log(users);
-    return users;
-  }
+    constructor() {}
+    async createUser(userData: User): Promise<User> {
+        console.log('clerkId', userData.clerkId);
+        console.log('email', userData.email);
+        console.log('id', userData.id);
+
+        const createUser = await prisma.user.create({
+            data: {
+                clerkId: userData.clerkId,
+                email: userData.email,
+                id: userData.id,
+            },
+        });
+        return createUser;
+    }
+
+    async getUsers(): Promise<User[]> {
+        return prisma.user.findMany();
+    }
 }
